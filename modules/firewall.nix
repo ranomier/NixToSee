fa162a9f9dff9d09dd248cd6805187b9d5a92da1
Ranomier <>
2025-01-15 17:40:52 +0100

    325 current  2025-01-15 17:40:45  24.11.20250106.3f0a8ac  6.12.2-zen1                          *

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -3,5 +3,5 @@
   networking.firewall.allowedTCPPorts = [8080 10001 10002];
   networking.firewall.allowedUDPPorts = [8080 10001 10002];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 }
