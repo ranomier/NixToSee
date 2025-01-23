4c77c7fa05091099e21efe5931302d00c0d5ef62
Ranomier <>
2025-01-23 04:45:19 +0100

    turned on auto login and cleaned up the file

--- modules/sec_auth/login-manager.nix
+++ modules/sec_auth/login-manager.nix
@@ -1,21 +1,30 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+let
+  default_command = "Hyprland";
+in
+{
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
   services.greetd = {
     enable = true;
     vt = 7;
     settings = {
+      initial_session = {
+        command = default_command;
+        user = "ranomier";
+      };
       default_session = {
-        command = ''          ${pkgs.greetd.tuigreet}/bin/tuigreet \
-                              --time \
-                              --time-format '%Y-%m-%dT%H:%M:%S %A' \
-                              --remember \
-                              --user-menu \
-                              --user-menu-min-uid 1000 \
-                              --user-menu-max-uid 2000 \
-                              --theme 'border=lightgreen;text=lightgreen;prompt=lightgreen;time=lightgreen;action=lightgreen;button=lightgreen;input=lightgreen' \
-                              --cmd Hyprland'';
-        # removed elements from theme: container
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+            --time \
+            --time-format '%Y-%m-%dT%H:%M:%S %A' \
+            --remember \
+            --user-menu \
+            --user-menu-min-uid 1000 \
+            --user-menu-max-uid 2000 \
+            --theme 'border=lightgreen;text=lightgreen;prompt=lightgreen;time=lightgreen;action=lightgreen;button=lightgreen;input=lightgreen' \
+            --cmd ${default_command}
+        '';
         user = "greeter";
       };
     };
