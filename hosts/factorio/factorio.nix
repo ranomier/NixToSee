d05d62a5a0b92b5ed944b2ab850b5397d1f47bd5
Ranomier <>
2025-01-23 04:50:10 +0100

    changed things with factorio server, but i am still not happy

--- hosts/factorio/factorio.nix
+++ hosts/factorio/factorio.nix
@@ -1,7 +1,7 @@
-{
-  pkgs,
-  lib,
-  ...
+{ pkgs
+, lib
+, modulesPath
+, ...
 }: {
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -9,62 +9,39 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  nixpkgs = {
-    # You can add overlays here
-    #overlays = with inputs.self.overlays; [
-    # Add overlays your own flake exports (from overlays and pkgs dir):
-    #additions
-    #modifications
-    #unstable-packages
-
-    # You can also add overlays exported from other flakes:
-    # neovim-nightly-overlay.overlays.default
-
-    # Or define it inline, for example:
-    # (final: prev: {
-    #   hi = final.hello.overrideAttrs (oldAttrs: {
-    #     patches = [ ./change-hello-to-hi.patch ];
-    #   });
-    # })
-    #];
-    # Configure your nixpkgs instance
-    config = {
+  nixpkgs.config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-    };
   };
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # https://lix.systems/ Lix is a modern, delicious implementation of the Nix package manager,
   # focused on correctness, usability, and growth –
   # and committed to doing right by its community.
-  #nix.package = pkgs.lix;
+  nix.package = pkgs.lix;
+
   imports = [
     #nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
     #./hardware-configuration.nix
-    ./boot.nix
-    ./hardware-configuration.nix
+    #./boot.nix
+    #./hardware-configuration.nix
 
     ../../modules/locale.nix
     #../../modules/game/server/factorio/factorio.nix
 
-    #<nixpkgs/nixos/modules/profiles/perlless.nix>
-    #<nixpkgs/nixos/modules/profiles/headless.nix>
-    #<nixpkgs/nixos/modules/profiles/minimal.nix>
-
+    (modulesPath + "/profiles/perlless.nix")
+    (modulesPath + "/profiles/minimal.nix")
     {
-      programs.command-not-found.enable = false;
-      environment.defaultPackages = [];
-      xdg = {
-        icons.enable = false;
-        mime.enable = false;
-        sounds.enable = false;
-      };
-      documentation.man.enable = false;
-      documentation.nixos.enable = false;
+      environment.defaultPackages = [ ]; 
+      boot.kernel.enable = false;
+      boot.isContainer = true;
+      nixpkgs.overlays = [ (self: super: {}) ];
+    }
+    {
+
       # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
       # (the default) this is the recommended approach. When using systemd-networkd it's
       # still possible to use this option, but it's recommended to use it in conjunction
@@ -73,8 +50,10 @@
       # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
     }
   ];
-  #disabledModules = [
+  disabledModules = [
+    (modulesPath + "/profiles/all-hardware.nix")
+    (modulesPath + "/profiles/base.nix")
   #  <nixpkgs/nixos/modules/profiles/all-hardware.nix>
   #  <nixpkgs/nixos/modules/profiles/base.nix>
-  #];
+  ];
 }
