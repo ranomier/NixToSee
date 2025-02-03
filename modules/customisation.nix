3fdd16f869ba6edf08720867ee0928c1a8026402
Ranomier <>
2025-02-03 18:32:14 +0100

    comments

--- modules/customisation.nix
+++ modules/customisation.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
+  # install and set neovim as MANPAGER
   environment.systemPackages = [pkgs.neovim];
   environment.variables = {
     "MANPAGER" = "nvim +Man!";
+
+    # set neovim as default editor
     "EDITOR" = "nvim";
   };
 }
