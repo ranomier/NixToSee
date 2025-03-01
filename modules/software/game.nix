b8aa94fcdc670b25b1814cd91a51efd7884a5130
Ranomier <>
2025-03-01 20:35:14 +0100

    formating

--- modules/software/game.nix
+++ modules/software/game.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
 
@@ -15,14 +15,14 @@
     gamescopeSession.enable = true;
 
     # make proton-ge alway available
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    extraCompatPackages = with pkgs; [proton-ge-bin];
   };
 
   # steam-run: Run commands in the same FHS environment that is used for Steam
-  environment.systemPackages = with pkgs; [ steam-run ];
+  environment.systemPackages = with pkgs; [steam-run];
 
   # Note that this is already enabled with programs.steam.enable = true;
   # but it might be usefull on systems without steam,
   # while still using steam hardware
   #hardware.steam-hardware.enable = true;
-  }
+}
