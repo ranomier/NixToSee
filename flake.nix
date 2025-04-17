7f094b80c5b4496485c81b4e6b3d0e1f659d9036
Ranomier <>
2025-04-17 22:48:09 +0200

    refactor and add jitsi

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
     } @ inputs: let
       # Supported systems for your flake packages, shell, etc.
       systems = [
-        "aarch64-linux"
+        #"aarch64-linux"
         "x86_64-linux"
       ];
 
@@ -53,9 +53,9 @@
 
       host_helper = hostname: {
         ${hostname} = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;} // {inherit nixos-unstable;};
+          specialArgs = {inherit inputs;};
           modules = [
-            ./hosts/${hostname}/${hostname}.nix
+            ./hosts/${hostname}
           ];
         };
       };
@@ -63,64 +63,24 @@
       # Your custom packages
       # Accessible through 'nix build', 'nix shell', etc
       #packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
       # Formatter for your nix files, available through 'nix fmt'
       # Other options beside 'alejandra' include 'nixpkgs-fmt'
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
       # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays {inherit inputs;};
+
       # Reusable nixos modules you might want to export
       # These are usually stuff you would upstream into nixpkgs
       #nixosModules = import ./modules/nixos;
-      # Reusable home-manager modules you might want to export
-      # These are usually stuff you would upstream into home-manager
-      #homeManagerModules = import ./modules/home-manager;
 
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
+      # to add more append // (host_helper example);
       nixosConfigurations =
-        host_helper "crocoite"; # // (host_helper example);
+        host_helper "crocoite" //
+        host_helper "jitsi";
 
-      ## Standalone home-manager configuration entrypoint
-      ## Available through 'home-manager --flake .#your-username@your-hostname'
-      #homeConfigurations = {
-      #  # FIXME replace with your username@hostname
-      #  "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
-      #    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-      #    extraSpecialArgs = {inherit inputs;};
-      #    modules = [
-      #      # > Our main home-manager configuration file <
-      #      ./home-manager/home.nix
-      #    ];
-      #  };
-      #};
-
-      packages.x86_64-linux = {
-        factorio = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          modules = [
-            # you can include your own nixos configuration here, i.e.
-            ./hosts/factorio/factorio.nix
-          ];
-
-          customFormats = {
-            "myFormat" = {
-              lib,
-              modulesPath,
-              ...
-            }: {
-              imports = [
-                "${toString modulesPath}/virtualisation/proxmox-lxc.nix"
-              ];
-              boot.loader.grub.enable = lib.mkForce false;
-              boot.loader.systemd-boot.enable = lib.mkForce false;
-              formatAttr = "tarball";
-              fileExtension = "asd.tar.xz";
-            };
-          };
-          format = "myFormat";
-          #format = "vmware";
-        };
-      };
     };
 }
