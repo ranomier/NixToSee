ddf81549332db3af30f0bb9cc29f89f122576a72
Ranomier <>
2025-05-08 23:45:04 +0200

    feat: added proper hardware-configuration and sshd

--- hosts/game-luanti/default.nix
+++ hosts/game-luanti/default.nix
@@ -1,13 +1,16 @@
 {
-  pkgs,
-  lib,
-  modulesPath,
-  ...
-}: {
   imports = [
     ./hardware-configuration.nix
 
     ../../system_profiles/defaults.nix
     ../../system_profiles/mini-container.nix
   ];
+
+  users = let
+    username = "root";
+  in {
+    users."${username}".openssh.authorizedKeys.keyFiles = [
+      /etc/nixos/ssh/authorized_keys.d/${username}
+    ];
+  };
 }
