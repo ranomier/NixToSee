14a58c86d1b0b1188c584312dfe66a7f4ddf4791
Ranomier <>
2025-05-15 20:05:29 +0200

    feat: added nvimdiff and nvimdiff

--- modules/software/neovim.nix
+++ modules/software/neovim.nix
@@ -1,4 +1,18 @@
-{
+{pkgs, ...}: let
+  makeDiff = name:
+    pkgs.writeShellScriptBin name ''
+      #!/usr/bin/env bash
+      if [ $# -lt 2 ]; then
+        echo "Usage: ${name} <file1> <file2> [more args…]" >&2
+        exit 1
+      fi
+      exec nvim -d "$@"
+    '';
+in {
+  environment.systemPackages = [
+    (makeDiff "vimdiff")
+    (makeDiff "nvimdiff")
+  ];
   programs.neovim = {
     enable = true;
     viAlias = true;
