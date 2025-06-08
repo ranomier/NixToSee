71487763a6243d638454913f92f33376de05b931
Ranomier <>
2025-06-08 23:11:43 +0200

    fix: sort and doc

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
     # Main nix package repository
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # NixOS unstable
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # ready made hardware configurations. e.G.: Power saving
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # generating filesystems in different formats
     nixos-generators = {
@@ -14,11 +15,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NixOS unstable channel
-    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # ready made hardware configurations. e.G.: Power saving
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # additional user specific nix modules
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # for managing flatpaks, like which ones are installed and which not
     nix-flatpak.url = "github:gmodena/nix-flatpak";
