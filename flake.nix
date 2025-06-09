f9acc609b265e1a04d84505a2fc5bf45cb3fd8a4
Ranomier <>
2025-06-09 20:52:04 +0200

    feat: reenable home-manager

--- flake.nix
+++ flake.nix
@@ -16,10 +16,10 @@
     };
 
     # additional user specific nix modules
-    #home-manager = {
-    #  url = "github:nix-community/home-manager/release-25.05";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # theming
     stylix = {
