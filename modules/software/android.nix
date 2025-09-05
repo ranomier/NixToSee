461a4b0146ae3a1661bcb8fffb209c99e44418bd
Ranomier <>
2025-09-06 00:45:42 +0200

    feat: add current (nixos-unstabe) version of waydroid-helper

--- modules/software/android.nix
+++ modules/software/android.nix
@@ -1,5 +1,7 @@
-{
+{pkgs, ...}: {
   # for running android apps
   # also starts the systemd service waydroid-container
   virtualisation.waydroid.enable = true;
+
+  environment.systemPackages =  [ pkgs.unstable.waydroid-helper ];
 }
