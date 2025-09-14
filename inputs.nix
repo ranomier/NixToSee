280936542e068d161cf237a55e7bcf406cebd04c
Ranomier <>
2025-09-14 22:48:19 +0200

    feat: mv flake.nix inputs.nix and add sym link
    Since my flake basically only contains inputs i though it to be fitting
    Also i have already an outputs.nix

---
+++ inputs.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    # Main nix package repository
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # NixOS unstable
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # ready made hardware configurations. e.G.: Power saving
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    # generating filesystems in different formats
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # additional user specific nix modules
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # theming
+    stylix = {
+      url = "github:danth/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-secrets = {
+      url = "path:/home/ranomier/Projects/nix-secrets";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # for managing flatpaks, like which ones are installed and which not
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+  };
+
+  outputs = inArgs: import ./outputs.nix inArgs;
+}
