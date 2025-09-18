683bcee5617e5a21d271e6093fc271ecaca633e5
Ranomier <>
2025-09-18 23:52:19 +0200

    fix: add lib again (what did i thought xD)

--- modules/dhcp-all-interfaces.nix
+++ modules/dhcp-all-interfaces.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   # Enables DHCP on each ethernet and wireless interface.
   # In case of scripted networking (the default) this is the recommended approach.
   # When using systemd-networkd it's still possible to use this option,
