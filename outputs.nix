b2e10ce9fe7982745aeafa274af3a31211408a96
Ranomier <>
2025-09-24 19:43:25 +0200

    fix: sort

--- outputs.nix
+++ outputs.nix
@@ -8,23 +8,12 @@ inArgs: let
   # pass to it, with each system as an argument
   forAllSystems = inArgs.nixpkgs.lib.genAttrs systems;
 in {
-  # NixOS configuration entrypoint
-  # Available through 'nixos-rebuild --flake .#your-hostname'
-  nixosConfigurations = import ./hosts inArgs;
-
-
-  # Your custom packages
-  # Accessible through 'nix build', 'nix shell', etc
-  #packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-
-
-  # Your custom packages and modifications, exported as overlays
-  overlays = import ./overlays inArgs;
-
-
-  # Reusable nixos modules you might want to export
-  # These are usually stuff you would upstream into nixpkgs
-  #nixosModules = import ./modules/nixos;
+  # Checks, they use derivations and run duing `nix flake check`,
+  # individual checks can be accessed trhough:
+  # `nix build -L -v --print-out-paths --no-link .#checks.x86_64-linux.<check-name>`
+  # list of checks defined under outputs.checks with:
+  # `nix eval --json .#checks`
+  checks = import ./checks inArgs;
 
 
   # Formatter for your nix files, available through 'nix fmt'
@@ -32,10 +21,21 @@ in {
   formatter = forAllSystems (system: inArgs.nixpkgs.legacyPackages.${system}.alejandra);
 
 
-  # Checks, they use derivations and run duing `nix flake check`,
-  # individual checks can be accessed trhough:
-  # `nix build -L -v --print-out-paths --no-link .#checks.x86_64-linux.<check-name>`
-  # list of checks defined under outputs.checks with:
-  # `nix eval --json .#checks`
-  checks = import ./checks inArgs;
+  # NixOS configuration entrypoint
+  # Available through 'nixos-rebuild --flake .#your-hostname'
+  nixosConfigurations = import ./hosts inArgs;
+
+
+  # Reusable nixos modules you might want to export
+  # These are usually stuff you would upstream into nixpkgs
+  #nixosModules = import ./modules/nixos;
+
+
+  # Your custom packages and modifications, exported as overlays
+  overlays = import ./overlays inArgs;
+
+
+  # Your custom packages
+  # Accessible through 'nix build', 'nix shell', etc
+  #packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 }
