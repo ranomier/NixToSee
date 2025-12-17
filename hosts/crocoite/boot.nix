dcfeac70eaaa852f8270508afe96753d4d606e56
Ranomier <>
2025-12-18 00:33:54 +0100

    feat: format + reorganisation

--- hosts/crocoite/boot.nix
+++ hosts/crocoite/boot.nix
@@ -1,8 +1,11 @@
 {
   boot.loader = {
-    systemd-boot.enable = true;
-    systemd-boot.configurationLimit = 20;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 20;
+    };
     efi.canTouchEfiVariables = true;
+
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
