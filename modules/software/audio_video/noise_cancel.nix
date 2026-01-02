4422d0f23ddee4bf9670ab15353ad84dbc8f1a11
Ranomier <>
2026-01-02 17:43:26 +0100

    feat: turn rnnoise off if there is no app requiring it

--- modules/software/audio_video/noise_cancel.nix
+++ modules/software/audio_video/noise_cancel.nix
@@ -3,9 +3,12 @@
     "context.modules" = [
       {
         "name" = "libpipewire-module-filter-chain";
+
         "args" = {
           "node.description" = "Noise Canceling source";
           "media.name" = "Noise Canceling source";
+          "audio.position" = ["FL" "FR"];
+
           "filter.graph" = {
             "nodes" = [
               {
@@ -15,25 +18,36 @@
                 # mono can be replaced with stereo for double the performance as cost
                 "label" = "noise_suppressor_mono";
                 "control" = {
-                  # if probability of sound being a voice is lower than this threshold - it will be silenced. In most cases the threshold between 85% - 95% would be fine. Without the VAD some loud noises may still be a bit audible when there is no voice.
+                  # if probability of sound being a voice is lower than this threshold it will be silenced.
+                  # In most cases the threshold between 85% - 95% would be fine.
+                  # Without the VAD some loud noises may still be a bit audible when there is no voice.
                   "VAD Threshold (%)" = 75.0;
-                  # for how long after the last voice detection the output won't be silenced. This helps when ends of words/sentences are being cut off.
+                  # for how long after the last voice detection the output won't be silenced.
+                  # This helps when ends of words/sentences are being cut off.
                   "VAD Grace Period (ms)" = 200;
-                  # similar to VAD Grace Period (ms) but for starts of words/sentences. This introduces latency!
+                  # similar to VAD Grace Period (ms) but for starts of words/sentences.
+                  # This introduces latency!
                   "Retroactive VAD Grace Period (ms)" = 0;
                 };
               }
             ];
           };
-          "audio.position" = [
-            "FL"
-            "FR"
-          ];
+
+
           "capture.props" = {
             "node.name" = "effect_input.rnnoise";
             "node.passive" = true;
             "audio.rate" = 48000;
+
+            # Only turns the rnnoise plugin on if an app requires it,
+            # from the noice canceling capture
+            "filter.smart" = true;
+            "filter.smart.name" = "rnnoise";
+            "filter.smart.target" = {
+              "media.name" = "Noise Canceling source";
+            };
           };
+
           "playback.props" = {
             "node.name" = "effect_output.rnnoise";
             "media.class" = "Audio/Source";
