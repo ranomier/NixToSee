5e3ea859df06f75048e33e635e219812b310ed10
Ranomier <>
2026-01-25 06:20:15 +0100

    chore: better name xdg-user-dirs.nix

--- modules/default.nix
+++ modules/default.nix
@@ -7,11 +7,11 @@
     # should only be imported directly or via system_profiles
     #./dhcp-all-interfaces.nix
 
-    ./environment.nix
     ./firewall.nix
     ./locale.nix
     ./networking.nix
     #./plymouth.nix # increases boot time too much
     ./theming.nix
+    ./xdg-user-dirs.nix
   ];
 }
