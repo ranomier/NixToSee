59ae103bf8601c26e5cf2b28bf994b5863e7f635
Ranomier <>
2026-02-06 06:24:28 +0100

    feat: add flake url and more doc

--- modules/software/nix-helper/nh.nix
+++ modules/software/nix-helper/nh.nix
@@ -1,8 +1,20 @@
 {
   programs.nh = {
     enable = true;
-    #clean.enable = true;
-    #clean.extraArgs = "--keep-since 4d --keep 3";
-    #flake = "/home/user/my-nixos-config";
+
+    flake = "/home/ranomier/Projects/NixOS";
+
+    #clean = {
+    #  enable = true;
+    #  extraArgs = "
+    #    --keep-since 4d  # At least keep gcroots and generations in this time range since now
+    #    --keep 3         # At least keep this number of generations
+    #    --no-gc          # Don't run nix store --gc
+    #    --no-gcroots     # Don't clean gcroots
+    #    --optimise       # Run nix-store --optimise after gc
+    #    --max <MAX>      # Pass --max to nix store gc
+    #  ";
+    #};
+
   };
 }
